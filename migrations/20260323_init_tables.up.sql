938b29be03aa587d85973093a2fb2fd6108193c1
olegdayo <ol-sidorenkov@mail.ru>
2026-03-29 18:49:56 +0300

    fix

--- migrations/20260323_init_tables.up.sql
+++ migrations/20260323_init_tables.up.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS mlist.models (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     repository TEXT NOT NULL,
-    version TEXT NOT TULL
+    version TEXT NOT NULL
 );
